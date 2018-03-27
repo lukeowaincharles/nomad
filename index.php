75bdf069278f77cb85581f202d5495d9be48f35b
unknown <lukeowaincharles@gmail.com>
2018-03-27 20:05:03 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+        <meta name="description" content="">
+
+        <link rel="stylesheet" href="./static/ext/bootstrap-3.3.7/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="./static/ext/bootstrap-3.3.7/css/bootstrap.min.css">
+        <title>Luke Owain Charles | Portfolio</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="masthead">
+                <h1 class="web__title">Luke Owain Charles</h1>
+                <nav>
+
+                </nav>
+            </div>
+            <main>
+                <div class="content__wrapper">
+                    <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim sit amet risus vitae ultricies. Pellentesque pharetra aliquam neque, et fringilla mauris mattis sagittis. In sit amet faucibus ex. Duis semper libero vitae ligula auctor posuere. Phasellus at nisi ex. Sed sit amet condimentum velit. Donec maximus ligula tincidunt, porta lectus ut, tincidunt orci.
+
+                        In eget ligula lacus. Aliquam cursus felis a libero condimentum porta. Vivamus eget eros odio. Aliquam varius ipsum ut felis facilisis, non tincidunt elit condimentum. Suspendisse id viverra eros. Curabitur rhoncus enim non malesuada efficitur. Curabitur faucibus elit vel dignissim commodo. Aenean scelerisque augue lorem, in molestie dui fermentum ut.</p>
+                </div>
+            </main>
+        </div>
+        <div class="main__image__wrapper">
+            <img srcset="" alt=""/>
+        </div>
+
+        <script src="./static/js/broozr.js"></script>
+        <script src="./static/js/modernizr-custom.js"></script>
+    </body>
+</html>
