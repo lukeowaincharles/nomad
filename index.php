554c2513af61c0ab2bf3b019d2f96e9df861a4e2
unknown <lukeowaincharles@gmail.com>
2018-04-05 11:49:05 +0100

    add image srcset, font and change markup

--- index.php
+++ index.php
@@ -7,30 +7,40 @@
 
         <link rel="stylesheet" href="./static/ext/bootstrap-3.3.7/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="./static/ext/bootstrap-3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lora|Source+Sans+Pro" rel="stylesheet">
+        <link rel="stylesheet" href="./static/css/master.css">
         <title>Luke Owain Charles | Portfolio</title>
     </head>
     <body>
-        <div class="container">
-            <div class="masthead">
-                <h1 class="web__title">Luke Owain Charles</h1>
-                <nav>
+        <div class="area__container">
+            <section class="area left">
+                <div class="masthead">
+                    <p class="web__title">Luke Owain Charles</p>
+                    <nav>
 
-                </nav>
-            </div>
-            <main>
-                <div class="content__wrapper">
-                    <h1>About</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim sit amet risus vitae ultricies. Pellentesque pharetra aliquam neque, et fringilla mauris mattis sagittis. In sit amet faucibus ex. Duis semper libero vitae ligula auctor posuere. Phasellus at nisi ex. Sed sit amet condimentum velit. Donec maximus ligula tincidunt, porta lectus ut, tincidunt orci.
-
-                        In eget ligula lacus. Aliquam cursus felis a libero condimentum porta. Vivamus eget eros odio. Aliquam varius ipsum ut felis facilisis, non tincidunt elit condimentum. Suspendisse id viverra eros. Curabitur rhoncus enim non malesuada efficitur. Curabitur faucibus elit vel dignissim commodo. Aenean scelerisque augue lorem, in molestie dui fermentum ut.</p>
+                    </nav>
                 </div>
-            </main>
-        </div>
-        <div class="main__image__wrapper">
-            <img srcset="" alt=""/>
-        </div>
+                <main>
+                    <div class="content__wrapper">
+                        <h1>About</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim sit amet risus vitae ultricies. Pellentesque pharetra aliquam neque, et fringilla mauris mattis sagittis. In sit amet faucibus ex. Duis semper libero vitae ligula auctor posuere. Phasellus at nisi ex. Sed sit amet condimentum velit. Donec maximus ligula tincidunt, porta lectus ut, tincidunt orci.
 
+                            In eget ligula lacus. Aliquam cursus felis a libero condimentum porta. Vivamus eget eros odio. Aliquam varius ipsum ut felis facilisis, non tincidunt elit condimentum. Suspendisse id viverra eros. Curabitur rhoncus enim non malesuada efficitur. Curabitur faucibus elit vel dignissim commodo. Aenean scelerisque augue lorem, in molestie dui fermentum ut.</p>
+                    </div>
+                </main>
+            </section>
+        
+        <section class="area right">
+            <div class="main__image__wrapper">
+                <img src="./static/img/tintagel-320w.jpg" 
+                     srcset="./static/img/tintagel-800w.jpg 800w, ./static/img/tintagel-1600w.jpg 1600w"
+                     sizes="(min-width: 992px) 1600px, (min-width: 768px) 800px"
+                     alt="Tintagel Cornwall"/>
+            </div>
+        </section>
+</div>
         <script src="./static/js/broozr.js"></script>
         <script src="./static/js/modernizr-custom.js"></script>
+        <script src="./static/js/master.js"></script>
     </body>
 </html>
